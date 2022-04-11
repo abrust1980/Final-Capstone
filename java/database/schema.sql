601e3ae92513b941425437b90ec12f11d4e48706
Amanda Brust <brustamandab@gmail.com>
2022-04-11 10:56:24 -0400

    Updating foreign key

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -36,7 +36,9 @@ CREATE TABLE book_detail (
 CREATE TABLE book_user (
 	user_id int,
 	isbn_number bigint
-
+	constraint fk_book_user_user_id foreign key (user_id) references user(user_id),
+	constraint fk_book_user_isbn_number foreign key (isbn_number) references book_detail(isbn_number),
+	constraint pk_book_user primary key (user_id, isbn_number)
 );
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
