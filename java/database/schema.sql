349eb097da361e40f05867b85a0b0b3be2de4532
Amanda Brust <brustamandab@gmail.com>
2022-04-10 22:35:55 -0400

    Pushing table changes with book_detail AB

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,7 +1,9 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS book_detail;
 DROP SEQUENCE IF EXISTS seq_user_id;
+DROP SEQUENCE IF EXISTS seq_book_id;
 
 CREATE SEQUENCE seq_user_id
   INCREMENT BY 1
@@ -18,8 +20,26 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE SEQUENCE seq_book_id
+	INCREMENT BY 1
+	NO MAXVALUE
+	NO MINVALUE
+	CACHE 1;
+
+CREATE TABLE book_detail (
+	book_id serial primary key,
+	author_last_name varchar(32) not null,
+	author_first_name varchar(32),
+	book_title varchar(255) not null,
+	publication_year bigint,
+	isbn_number bigint,
+	book_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+
+INSERT INTO book_detail (author_last_name, author_first_name, book_title, publication_year, isbn_number) VALUES ('Day', 'Felicia', 'Embrace Your Weird', 2019, 9781982113223);
 
 
 COMMIT TRANSACTION;
