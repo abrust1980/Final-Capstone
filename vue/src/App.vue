46902e1604fa08fa0ef42283257a168d0b905432
Felicity Homman <fhommanwriting@gmail.com>
2022-04-21 17:08:28 -0400

    CSS complete maybe

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -20,9 +20,9 @@
     </div>
     <router-view />
     <div id="footer-div">
-      <p class="footer-text">2022 © Pocket Librarian. All Rights Reserved.</p>
+      <p id="copyright" class="footer-text">2022 © Pocket Librarian. All Rights Reserved.</p>
       <p id="about-link" class="footer-text">ABOUT</p>
-            <div id="icons">
+        <div id="icons">
         <img src="../src/assets/icons8-facebook-64.png" />
         <img src="../src/assets/icons8-instagram-64.png" />
         <img src="../src/assets/icons8-twitter-64.png" />
@@ -33,17 +33,27 @@
 
 <style>
 #footer-div {
-  display: flex;
+  display: grid;
+  align-items: center;
+  grid-template-columns: 1fr 3fr 1fr;
+  grid-template-rows: auto;
+  grid-template-areas:
+  "copyright about images";
   background-color: #ECECEC;
-  font-size: 1vw;
   border-top: 5px solid #37464a;
   font-family: 'Comfortaa', cursive;
-  align-items: center;
-  
+}
+
+#copyright {
+  grid-area: copyright;
 }
 
 #about-link {
-  flex-grow: 2;
+  grid-area: about;
+}
+
+#icons {
+  grid-area: images;
 }
 
 #nav {
